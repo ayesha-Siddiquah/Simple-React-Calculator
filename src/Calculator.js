2197b62d7abf3b1e7072bb7acd0882b5af218562
silvio <suchysilvio@gmail.com>
2023-11-29 05:45:31 -0800

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+import { evaluate } from 'mathjs';
+import { calculatorButtons } from './calculator-bonus-03-button-data';
+
+const Calculator = () => {
+  const [display, setDisplay] = useState('');
+  const [memory, setMemory] = useState(0);
+
+  const handleButtonClick = (button) => {
+    const { type, value } = button;
+
+    switch (type) {
+      case 'clear':
+        if (value === 'Clear') {
+          setDisplay('');
+        } else if (value === 'All Clear') {
+          setDisplay('');
+          setMemory(0);
+        }
+        break;
+      case 'memory':
+        switch (value) {
+          case 'Memory Save':
+            setMemory(parseFloat(display) || 0);
+            break;
+          case 'Memory Clear':
+            setMemory(0);
+            break;
+          case 'Memory Recall':
+            setDisplay(memory.toString());
+            break;
+          case 'Memory Subtract':
+            setMemory(memory - (parseFloat(display) || 0));
+            break;
+          case 'Memory Addition':
+            setMemory(memory + (parseFloat(display) || 0));
+            break;
+          default:
+            break;
+        }
+        break;
+      case 'number':
+      case 'operator':
+        setDisplay((prevDisplay) => prevDisplay + value);
+        break;
+      case 'sign':
+        setDisplay((prevDisplay) => (-1 * (parseFloat(prevDisplay) || 0)).toString());
+        break;
+      case 'percent':
+        setDisplay((prevDisplay) => (parseFloat(prevDisplay) || 0) / 100);
+        break;
+      case 'sqrt':
+        setDisplay(Math.sqrt(parseFloat(display) || 0).toString());
+        break;
+      case 'enter':
+        calculateResult();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const calculateResult = () => {
+    try {
+      const result = evaluate(display || '0');
+      setDisplay(result.toString());
+    } catch (error) {
+      setDisplay('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{display || '0'}</div>
+      <div className="buttons">
+        {calculatorButtons.map((button) => (
+          <button
+            key={button.className}
+            className={`button ${button.className}`}
+            onClick={() => handleButtonClick(button)}
+          >
+            {button.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
